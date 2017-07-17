e820343b03ac14229acec7897e017ada499272dc
Leo <leowzukw@vmail.me>
2017-07-17 17:56:09 +0200

    Create listing by language

--- lib/listings.rb
+++ lib/listings.rb
@@ -1,3 +1,8 @@
 def db_entries_random_order
   @items.find_all('/profile/*').shuffle
 end
+
+def db_entries_by_lang(lang)
+  @items.find_all('/profile/*')
+    .select { |i| i[:lang].include? lang }
+end
